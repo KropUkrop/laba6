cd9bfc97750864a828a11136dd949a4bb6ffd8d3
KropUkrop <nastya.kropanina@gmail.com>
2024-12-16 20:50:31 +0300

    Update Zachetka.h

--- Zachetka.h
+++ Zachetka.h
@@ -9,9 +9,9 @@ private:
 
 public:
     Zachetka(const std::string& owner, const std::string& issue_date, int semesters);
-    void display() const override; // Переопределение виртуального метода
-    bool isValid() const override; // Реализация проверки действительности
-    void addSemester(); // Собственный метод
+    void display() const override; // РџРµСЂРµРѕРїСЂРµРґРµР»РµРЅРёРµ РІРёСЂС‚СѓР°Р»СЊРЅРѕРіРѕ РјРµС‚РѕРґР°
+    bool isValid() const override; // Р РµР°Р»РёР·Р°С†РёСЏ РїСЂРѕРІРµСЂРєРё РґРµР№СЃС‚РІРёС‚РµР»СЊРЅРѕСЃС‚Рё
+    void addSemester(); // РЎРѕР±СЃС‚РІРµРЅРЅС‹Р№ РјРµС‚РѕРґ
 };
 
 #endif // ZACHETKA_H
